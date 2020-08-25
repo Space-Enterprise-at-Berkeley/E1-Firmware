78c5ed291cb61ead5fbb275c8de1c8ffe8801788
Vainavi Viswanath <vainaviv2001@berkeley.edu>
2020-08-25 15:19:17 -0700

    The brain seems fine, just pushing it up

--- brain_I2C/brain_I2C.ino
+++ brain_I2C/brain_I2C.ino
@@ -1,1 +1,23 @@
+#include <Wire.h>
 
+//on lauchpad, I2C SCL is P4.2 and SDA is P4.1
+// on teensy, SCL is pin 19 and SDA is Pin 18
+
+void setup() {
+  Wire.begin();  // join i2c bus (address optional for master)
+  Serial.begin(9600);
+  Serial.println("finished setup");
+}
+
+void loop() {
+  Wire.requestFrom(8, 5);  // request 6 bytes from slave device #8
+  String response = "";
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.println(c);
+    //response += c;
+  }
+  //double response = Wire.read();
+  //Serial.println(response);
+  delay(1000); //frequency of request sent out
+}
