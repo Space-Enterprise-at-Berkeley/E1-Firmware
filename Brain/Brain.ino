03848625a24e41e35ec6bd93ed337e38e6d8c2e0
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-10-15 09:30:39 -0700

    change byte to int

--- Brain/Brain.ino
+++ Brain/Brain.ino
@@ -1,57 +1,85 @@
 /*
- * Brain_I2C.ino - A c++ program that uses I2C to establish communication between 
+ * Brain_I2C.ino - A c++ program that uses I2C to establish communication between
  * the sensors and valves inside to the rocket with the ground station. Able to send
- * data to the ground station via RF. Can receive and process commands sent from 
- * ground station. 
+ * data to the ground station via RF. Can receive and process commands sent from
+ * ground station.
  * Created by Vainavi Viswanath, Aug 21, 2020.
  */
 
 #include <Wire.h>
-#include <SoftwareSerial.h>
 #include "brain_utils.h"
+#include <GPS.h>
+#include <Barometer.h>
+#include <ducer.h>
 
-SoftwareSerial RFSerial(2,3);
+#define RFSerial Serial2
+#define GPSSerial Serial3
 
+
+// within loop state variables
 int board_address = 0;
-byte sensor_id = 0;
-uint8_t index = 0;
+int sensor_id = 0;
+uint8_t val_index = 0;
+String command = "";
 
 /*
  * Array of all sensors we would like to get data from.
  */
-sensorInfo all_ids[8] = {
-  sensorInfo("Low Pressure",8,0,1,1) //example
+sensorInfo all_ids[11] = {
+  // local sensors
+   {"LOX Injector Low Pressure",  -1, -1, 1, 1, &(Ducers::readLOXInjectorPressure)},
+   {"Prop Injector Low Pressure", -1, -1, 2, 1, &(Ducers::readPropaneInjectorPressure)},
+   {"LOX Tank Low Pressure",      -1, -1, 3, 1, &(Ducers::readLOXTankPressure)},
+   {"Prop Tank Low Pressure",     -1, -1, 4, 1, &(Ducers::readPropaneTankPressure)},
+   {"High Pressure",              -1, -1, 5, 2, &(Ducers::readHighPressure)},
+   {"Temperature",                -1, -1, 6, 3, NULL},
+   {"GPS",                        -1, -1, 7, 5, &(GPS::readPositionData)},
+   {"GPS Aux",                    -1, -1, 8, 8, &(GPS::readAuxilliaryData)},
+   {"Barometer",                  -1, -1, 8, 6, &(Barometer::readAltitudeData)},
+   {"Load Cell Engine Left",      -1, -1, 9,  5, NULL},
+   {"Load Cell Engine Right",     -1, -1, 10, 5, NULL}
 };
 
-sensorInfo sensor = sensorInfo("",0,0,0,0);
+sensorInfo sensor {"", 0, 0, 0, 0, 0};
 
-/* 
+/*
  *  Stores how often we should be requesting data from each sensor.
  */
 int sensor_checks[sizeof(all_ids)/sizeof(sensorInfo)][2];
 
 valveInfo valve_ids[7] = {
-  valveInfo("LOX 2 Way", 20) //example
+  valveInfo("LOX 2 Way", 20),
+  valveInfo("LOX 5 Way", 21),
+  valveInfo("LOX GEMS", 22),
+  valveInfo("Propane 2 Way", 23),
+  valveInfo("Propane 5 Way", 24),
+  valveInfo("Propane GEMS", 25),
+  valveInfo("High Pressure Solenoid", 26)
 };
 
 valveInfo valve = valveInfo("",0);
 
 void setup() {
-  Wire.begin();       
+  Wire.begin();
   Serial.begin(9600);
   RFSerial.begin(57600);
   for (int i=0; i<sizeof(all_ids)/sizeof(sensorInfo); i++) {
     sensor_checks[i][0] = all_ids[i].clock_freq;
     sensor_checks[i][1] = 1;
   }
+
+  Ducers::init(&Wire);
+  Barometer::init(&Wire);
+  GPS::init(&GPSSerial);
 }
 
-void loop() { 
-  String command = "";
-  command = RFSerial.readString();
-  int action = decode_received_packet(command, &valve);
-  take_action(&valve, action);
-  
+void loop() {
+  if(RFSerial.available()){
+    command = RFSerial.readString();
+    int action = decode_received_packet(command, &valve);
+    take_action(&valve, action);
+  }
+
   /*
    * Code for requesting data and relaying back to ground station
    */
@@ -65,24 +93,30 @@ void loop() {
     sensor = all_ids[j];
     board_address = sensor.board_address;
     sensor_id = sensor.sensor_id;
+    if(sensor_id != -1) {
+      Wire.beginTransmission(board_address);
+      Wire.write(sensor_id);
+      Wire.endTransmission();
+      Wire.requestFrom(board_address, 24);
 
-    Wire.beginTransmission(board_address);
-    Wire.write(sensor_id);
-    Wire.endTransmission();
-    Wire.requestFrom(board_address, 24); 
-  
-    index = 0;
-    while (Wire.available()){
-      farrbconvert.buffer[index] = Wire.read();
-      index++;
+      val_index = 0;
+      while (Wire.available()){
+        farrbconvert.buffer[val_index] = Wire.read();
+        val_index++;
+      }
+    } else {
+      sensor.dataReadFunc(farrbconvert.sensorReadings);
     }
     String packet = make_packet(sensor);
+    //Serial.println(packet);
     RFSerial.println(packet);
   }
   delay(100);
 }
 
+
 bool write_to_SD(String message){
   // every reading that we get from sensors should be written to sd and saved.
   // TODO: Someone's code here
+  return false;
 }
