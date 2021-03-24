72fb147388801312fbd2f84bd0725755e08fc238
daangbruce <68445988+daangbruce@users.noreply.github.com>
2021-03-23 21:25:08 -0700

    Load cell library (#47)
    Load Cell library

--- lib/loadCell/loadCell.h
+++ lib/loadCell/loadCell.h
@@ -1,40 +1,57 @@
 /**
  *
  * HX711 library for Arduino
- * https://github.com/bogde/HX711
  *
- * MIT License
- * (c) 2018 Bogdan Necula
- *
+ * Created by Bruce Deng, Feb 6, 2021.
 **/
+#ifndef __loadCell__
+#define __loadCell__
+
 #include <HX711.h>
 
 namespace LoadCell {
-		HX711 _lc1;
-		HX711 _lc2;
 
-		int calibrationValue1; // ======== SET THIS TO SOMETHING ===========
-		int calibrationValue2;
+		HX711 *_loadcells;
+		int _numSensors;
 
-		#define DOUT1 2
-		#define SCL1 3
+		byte  *_dout_pins;
+		byte  *_sck_pins;
+		float *_calibration_vals;
 
-		#define DOUT2 4
-		#define SCL2 5
+		int init(HX711 *loadcells, int numSensors, byte  *dout_pins,
+									byte * sck_pins, float  *calibration_vals) {
 
-    void init() {
-			_lc1.begin(DOUT1, SCL1);
-		  _lc1.set_scale(calibrationValue1); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-		  _lc1.tare();	//Assuming there is no weight on the scale at start up, reset the scale to 0
+      _loadcells = loadcells;
 
-			_lc2.begin(DOUT2, SCL2);
-			_lc2.set_scale(calibrationValue2);
-			_lc2.tare();
+			_numSensors = numSensors;
+			_dout_pins = dout_pins;
+			_sck_pins = sck_pins;
+			_calibration_vals = calibration_vals;
+
+//~~~~~~Needs to happen wherever loadcell.h is used~~~~~~~~~
+//     for (int i = 0; i < numSensors; i++) {
+//     loadcells[i].begin(doutPins[i], sckPins[i]);
+//     loadcells[i].set_scale(calVals[i]);
+//     loadcells[i].tare();
+//
+//     long zero_factor = loadcells[i].read_average(); //Get a baseline reading
+//     Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+//     Serial.println(zero_factor);
+//     }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+		 return 0;
 		}
 
-		void readLoad(float * data) {
-			data[0] = _lc1.get_units();
-			data[1] = _lc2.get_units();
-			data[2] = -1;
+		void readLoadCells(float *data) {
+
+			for (int i = 0; i < _numSensors; i++) {
+				data[i] = _loadcells[i].get_units() * 0.453592;
+			}
+			data[_numSensors] = -1;
 		}
+
 }
+
+#endif
