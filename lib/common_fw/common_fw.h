013cb4c94eb8c936391ff816c1b687e1335e6e02
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2021-03-26 23:43:40 -0700

    I think it fucking works

--- lib/common_fw/common_fw.h
+++ lib/common_fw/common_fw.h
@@ -50,10 +50,6 @@ struct Queue {
 
   uint8_t dequeue(char * buffer) {
     if(length > 0) {
-      #ifdef DEBUG
-          Serial.println("dequeuing, curr size " + String(length));
-          Serial.flush();
-      #endif
       length--;
 
       strncpy(buffer, q[front].message, q[front].length + 1);
