0566b4e6f592866ae9bd7dd492f5fde3d394c89d
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2021-04-16 22:29:27 -0700

    add debug

--- src/ActuatorController/ActuatorController.cpp
+++ src/ActuatorController/ActuatorController.cpp
@@ -195,6 +195,11 @@ void loop() {
     sensorReadFunc(sensor->id);
     debug("finished sensor read");
     packet = make_packet(sensor->id, false);
+    for (int i = 0; i < maxReadings; i++){
+      Serial.print(farrbconvert.sensorReadings[i]);
+      Serial.print(", ");
+    }
+    Serial.println("");
     Serial.println(packet);
     #ifdef ETH
     sendEthPacket(packet.c_str());
