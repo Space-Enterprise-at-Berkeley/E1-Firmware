8c40f665df2856303c46ceaf0ee8df05940bc91c
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2021-04-20 17:00:23 -0700

    fix compile errors

--- src/E1_waterflow_v2/E1_waterflow_v2.cpp
+++ src/E1_waterflow_v2/E1_waterflow_v2.cpp
@@ -155,11 +155,11 @@ void loop() {
     receivedCommand = false;
   }
 
-  if (Automation::_eventList->length > 0) {
-    Serial.print(Automation::_eventList->length);
+  if (Automation::_eventList.length > 0) {
+    Serial.print(Automation::_eventList.length);
     Serial.println(" events remain");
-    Automation::autoEvent* e = &(Automation::_eventList->events[0]);
-    if (millis() - Automation::_eventList->timer > e->duration) {
+    Automation::autoEvent* e = &(Automation::_eventList.events[0]);
+    if (millis() - Automation::_eventList.timer > e->duration) {
 
       e->action();
 
@@ -184,7 +184,7 @@ void loop() {
 
       Automation::removeEvent();
       //reset timer
-      Automation::_eventList->timer = millis();
+      Automation::_eventList.timer = millis();
     }
   }
 
