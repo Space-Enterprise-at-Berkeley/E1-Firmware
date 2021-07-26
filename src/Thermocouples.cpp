23e588c15cfd521c2593d6c4b112fa4ac9011ad6
Nolan <nlautrette@berkeley.edu>
2021-07-26 12:57:30 -0700

    small fixes

--- src/Thermocouples.cpp
+++ src/Thermocouples.cpp
@@ -5,10 +5,10 @@ namespace Thermocouples
 
   //init function for a TC instance
   //taken from old code
-  int Thermocouples::init(Adafruit_MCP9600 *cryo_boards, uint8_t * addrs, _themotype * types, TwoWire *theWire, float *latestReads) {
+  int Thermocouple::init(Adafruit_MCP9600 *cryo_board, uint8_t * addrs, _themotype * type, TwoWire *theWire, float *latestRead) {
      _addrs = addrs;
-     _latestReads = latestReads;
-     _cryo_amp_boards = cryo_boards;
+     _latestRead = latestRead;
+     _cryo_amp_board = cryo_board;
 
 
     if (!_cryo_amp_board.begin(addrs, theWire)) {
@@ -16,35 +16,37 @@ namespace Thermocouples
          return -1;
        }
 
-    _cryo_amp_boards.setADCresolution(MCP9600_ADCRESOLUTION_16);
-    _cryo_amp_boards.setThermocoupleType(types);
-    _cryo_amp_boards.setFilterCoefficient(0);
-    _cryo_amp_boards.enable(true);
+    _cryo_amp_board.setADCresolution(MCP9600_ADCRESOLUTION_16);
+    _cryo_amp_board.setThermocoupleType(type);
+    _cryo_amp_board.setFilterCoefficient(0);
+    _cryo_amp_board.enable(true);
 
 
     Serial.println(i);
-    Serial.println(_latestReads);
-    _latestReads[i] = -1;
-    Serial.println(_latestReads);
-    _latestReads[i] = _cryo_amp_boards.readThermocouple();
-    Serial.println(_latestReads);
+    Serial.println(_latestRead);
+    _latestRead = -1;
+    Serial.println(_latestRead);
+    _latestRead = _cryo_amp_board.readThermocouple();
+    Serial.println(_latestRead);
 
 
      return 0;
    }
 
-
-  void Thermocouples::run() {
+//function for reading value for TC amp.
+//record the timestamp and the read value; both of which are variables defined in the header file.
+  void Thermocouple::run() {
     value = cryo_amp_board.readThermocouple();
     timestamp = micros();
   }
 
-
-
   //creating Thermocouple instances, each corresponding to a specific TC used in the system.
-  Thermocouples TC1;
-  Thermocouples TC2;
-  Thermocouples TC3;
+  Thermocouple TC1;
+  Thermocouple TC2;
+  Thermocouple TC3;
+
+  //array of all Thermocouples
+  Thermocouple allThermocouples [] = {TC1, TC2, TC3};
 
   //function within Thermocouples namespace that intialiazes all Thermocouple instances, as defined above.
   void initializeAll() {
@@ -52,6 +54,10 @@ namespace Thermocouples
     TC2.init(/*parameters here*/);
     TC3.init(/*parameters here*/);
 
-    //How to add to scheduler?
+    //adding TC instances to the scheduer
+    Scheduler::repeatTask(&TC1, 200 * 1000);
+    Scheduler::repeatTask(&TC2, 200 * 1000);
+    Scheduler::repeatTask(&TC3, 200 * 1000);
+
   }
 }
--- src/Thermocouples.h
+++ src/Thermocouples.h
@@ -7,17 +7,19 @@
 namespace Thermocouples
 {
 
-  class Thermocouples: public Task
+  class Thermocouple: public Task
   {
   public:
     //taken from old code:
-    int init(Adafruit_MCP9600 *cryo_boards, uint8_t * addrs, _themotype * types, TwoWire *theWire, float *latestReads);
-    //taken straight from scheduler.h:
+    int init(Adafruit_MCP9600 *cryo_board, uint8_t * addrs, _themotype * types, TwoWire *theWire, float *latestReads);
+    //implementing run from Task class
     void run();
     uint32_t timestamp;
     uint32_t value;
   private:
     Adafruit_MCP9600 * cryo_amp_board;
+    uint8_t * _addrs;
+    float * _latestRead;
   };
 
 
