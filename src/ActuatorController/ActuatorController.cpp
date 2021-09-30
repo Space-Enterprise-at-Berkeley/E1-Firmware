c050b635323300c7b9c3902ade71fa41b46e5074
Justin <justinxyang@berkeley.edu>
2021-09-29 17:57:49 -0700

    Modify AcutatorController code to account for Automation::_eventList change in common library

--- src/ActuatorController/ActuatorController.cpp
+++ src/ActuatorController/ActuatorController.cpp
@@ -149,11 +149,11 @@ void loop() {
   }
 
 
-  if (Automation::_eventList->length > 0) {
-    Serial.print(Automation::_eventList->length);
+  if (Automation::_eventList.length > 0) {
+    Serial.print(Automation::_eventList.length);
     Serial.println(" events remain");
-    Automation::autoEvent* e = &(Automation::_eventList->events[0]);
-    if (millis() - Automation::_eventList->timer > e->duration) {
+    Automation::autoEvent* e = &(Automation::_eventList.events[0]);
+    if (millis() - Automation::_eventList.timer > e->duration) {
   
       e->action();
     
@@ -167,7 +167,7 @@ void loop() {
     
       Automation::removeEvent();
       //reset timer
-      Automation::_eventList->timer = millis();
+      Automation::_eventList.timer = millis();
     }
   }
 
