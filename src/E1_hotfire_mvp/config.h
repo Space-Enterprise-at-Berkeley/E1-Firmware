ce1f188aad659fc4f9a8ccb167b576739e5cecca
benjamint8 <bent8@berkeley.edu>
2021-11-10 20:15:57 -0800

    swapped control pins & INA219s for igniter and LED, to run igniter off of 24V instead, to prevent shorting between breakwire high and igniter high

--- src/E1_hotfire_mvp/config.h
+++ src/E1_hotfire_mvp/config.h
@@ -104,10 +104,10 @@ int stopThermoReadRate();
 
 // Solenoids
 const uint8_t numSolenoids = 8;   // l2, l5, lg, p2, p5, pg, h, h enable
-uint8_t solenoidPins[numSolenoids] = {5,  3,  1,  4,  2,  0, 6, 39};
+uint8_t solenoidPins[numSolenoids] = {5,  3,  1,  4,  0,  2, 6, 39};
 const uint8_t numSolenoidCommands = 10;    //       l2, l5, lg, p2, p5, pg,  h, arm, launch , h enable
 uint8_t solenoidCommandIds[numSolenoidCommands] = {20, 21, 22, 23, 24, 25, 26,  27, 28     , 31};
-uint8_t solenoidINAAddrs[numSolenoids] = {0x40, 0x42, 0x44, 0x41, 0x43, 0x45};
+uint8_t solenoidINAAddrs[numSolenoids] = {0x40, 0x42, 0x44, 0x41, 0x45, 0x43};
 float maxSolenoidCurrent = 1.0;
 
 LTC4151 pressurantSolenoidMonitor;
