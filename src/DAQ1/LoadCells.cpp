8ecf4f20849582b78cc5e5d6843112b90cfad8a9
Andy Cate <and164v@gmail.com>
2022-01-20 20:44:38 -0800

    load cell calibration

--- src/DAQ1/LoadCells.cpp
+++ src/DAQ1/LoadCells.cpp
@@ -5,16 +5,16 @@ namespace LoadCells {
     float loadCell2Value;
 
     void initLoadCells() {
-        HAL::lcAmp0.set_scale(4000);
-        HAL::lcAmp1.set_scale(-4000);
+        HAL::lcAmp0.set_scale(-2520);
+        HAL::lcAmp1.set_scale(2660);
 
         HAL::lcAmp0.tare();
         HAL::lcAmp1.tare();
     }
 
     uint32_t sampleLoadCells() {
-        loadCell1Value = HAL::lcAmp0.get_units();
-        loadCell2Value = HAL::lcAmp1.get_units();
+        loadCell1Value = HAL::lcAmp0.get_units(); // in pounds
+        loadCell2Value = HAL::lcAmp1.get_units(); // in pounds
 
         Comms::Packet tmp = {.id = 120};
         Comms::packetAddFloat(&tmp, loadCell1Value);
