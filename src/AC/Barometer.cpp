2b6a2b4d1d5322fc27a2da8e8e86614b70af3ada
Jewook Ryu <ryu_jewook@berkeley.edu>
2022-01-22 11:15:24 -0800

    created initial code structure

---
+++ src/AC/Barometer.cpp
@@ -0,0 +1,26 @@
+#include "Barometer.h"
+
+namespace Barometer {
+    Comms::Packet baroPacket = {.id = 21}; // TODO: check if ID is valid
+    uint32_t baroUpdatePeriod = 100 * 1000; // TODO: change placeholder
+
+    void init() {
+        bmp388.begin();
+    }
+    
+    uint32_t sampleAltPressTemp() {
+        bmp388.startForcedConversion(); // Start a forced conversion (if in SLEEP_MODE)
+        bool successfully_measured = bmp388.getMeasurements(temperature, pressure, altitude);
+        if (successfully_measured) {
+            baroPacket.len = 0;
+            Comms::packetAddFloat(&baroPacket, altitude);
+            Comms::packetAddFloat(&baroPacket, pressure);
+            Comms::packetAddFloat(&baroPacket, temperature);
+            Comms::emitPacket(&battPacket);
+
+            return baroUpdatePeriod;
+        } else {
+            // TODO
+        }
+    }
+}
--- src/AC/Barometer.h
+++ src/AC/Barometer.h
@@ -1,10 +1,27 @@
+/**
+ * @file Barometer.h
+ * @author Jewook Ryu (ryu_jewook@berkeley.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-22
+ * 
+ * @copyright Copyright (c) 2022
+ * @source: https://github.com/MartinL1/BMP388_DEV
+ */
+
 #pragma once
 
 #include <Common.h>
+#include <Comms.h>
 
 #include <Arduino.h>
 #include "BMP388_DEV.h"
 
 namespace Barometer {
+    extern float altitude, pressure, temperature;
+    extern BMP388_DEV bmp388;
 
+    void init();
+    // altitude in meters (m), pressure in hectoPascals/millibar (hPa), temperature in degrees celcius (°C)
+    uint32_t sampleAltPressTemp();
 }
