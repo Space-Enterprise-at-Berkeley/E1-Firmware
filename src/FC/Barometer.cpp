ea6f335a2f82f64346cb0178fde054dbd188d06c
Jewook Ryu <ryu_jewook@berkeley.edu>
2022-01-25 22:15:57 -0800

    now tries until successful reading

--- src/FC/Barometer.cpp
+++ src/FC/Barometer.cpp
@@ -13,25 +13,20 @@ namespace Barometer {
     
     uint32_t sampleAltPressTemp() {
         bmp388.startForcedConversion(); // Start a forced conversion (if in SLEEP_MODE)
-        bool successfully_measured = bmp388.getMeasurements(temperature, pressure, altitude);
-        if (successfully_measured) {
-            // baroPacket.len = 0;
-            // Comms::packetAddFloat(&baroPacket, altitude);
-            // Comms::packetAddFloat(&baroPacket, pressure);
-            // Comms::packetAddFloat(&baroPacket, temperature);
-            // Comms::emitPacket(&baroPacket);
-            Serial.print("temperature: ");
-            Serial.print(temperature);
-            Serial.print(", pressure: ");
-            Serial.print(pressure);
-            Serial.print(", altitude: ");
-            Serial.println(altitude);
-
-            // return baroUpdatePeriod;
-            return 0;
-        } else {
-            // TODO
-            Serial.println("FAIL!!");
+        bool successfully_measured = false;
+        while (!successfully_measured) {
+            successfully_measured = bmp388.getMeasurements(temperature, pressure, altitude);
         }
+        // baroPacket.len = 0;
+        // Comms::packetAddFloat(&baroPacket, altitude);
+        // Comms::packetAddFloat(&baroPacket, pressure);
+        // Comms::packetAddFloat(&baroPacket, temperature);
+        // Comms::emitPacket(&baroPacket);
+        Serial.print("temperature: ");
+        Serial.print(temperature);
+        Serial.print(", pressure: ");
+        Serial.print(pressure);
+        Serial.print(", altitude: ");
+        Serial.println(altitude);
     }
 }
--- src/tests/baro-test/main.cpp
+++ src/tests/baro-test/main.cpp
@@ -13,22 +13,21 @@ int main() {
 
     while(1) {
         bmp388.startForcedConversion(); // Start a forced conversion (if in SLEEP_MODE)
-        bool successfully_measured = bmp388.getMeasurements(temperature, pressure, altitude);
-        if (successfully_measured) {
-            // baroPacket.len = 0;
-            // Comms::packetAddFloat(&baroPacket, altitude);
-            // Comms::packetAddFloat(&baroPacket, pressure);
-            // Comms::packetAddFloat(&baroPacket, temperature);
-            // Comms::emitPacket(&baroPacket);
-            Serial.print("temperature: ");
-            Serial.print(temperature);
-            Serial.print(", pressure: ");
-            Serial.print(pressure);
-            Serial.print(", altitude: ");
-            Serial.println(altitude);
-        } else {
-            Serial.println("FAIL!!");
+        bool successfully_measured = false;
+        while (!successfully_measured) {
+            successfully_measured = bmp388.getMeasurements(temperature, pressure, altitude);
         }
+        // baroPacket.len = 0;
+        // Comms::packetAddFloat(&baroPacket, altitude);
+        // Comms::packetAddFloat(&baroPacket, pressure);
+        // Comms::packetAddFloat(&baroPacket, temperature);
+        // Comms::emitPacket(&baroPacket);
+        Serial.print("temperature: ");
+        Serial.print(temperature);
+        Serial.print(", pressure: ");
+        Serial.print(pressure);
+        Serial.print(", altitude: ");
+        Serial.println(altitude);
         delay(1000);
     }
     
