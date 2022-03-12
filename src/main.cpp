65319d2ba00ddaa698b0abf77eb415398786ec70
Justin <justinxyang@berkeley.edu>
2022-03-12 03:27:32 -0800

    added moving maximum for cap

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 #include <FDC2214.h>
 #include <TMP236.h>
 
+#include <CircularBuffer.h>
+
 #include <Comms.h>
 #include <Common.h>
- 
+
 #define STATUS_LED 34
 #define TEMP_PIN 1
 #define EN_485 20
 #define TE_485 19
 
 FDC2214 _capSens;
-TMP236 _tempSens = TMP236(TEMP_PIN);;
+TMP236 _tempSens = TMP236(TEMP_PIN);
 
 void setup()
 {
@@ -30,9 +32,13 @@ void setup()
   pinMode(STATUS_LED, OUTPUT);
   pinMode(17, OUTPUT);
 }
- 
-unsigned long previousMillis = 0;  
+
+unsigned long previousMillis = 0;
 const long interval = 25;
+
+const uint8_t logSecs = 8;
+
+CircularBuffer<float, (logSecs * 1000 / interval)> capBuffer;
 
 Comms::Packet capPacket = {.id = PACKET_ID};
 
@@ -43,16 +49,25 @@ void loop()
   digitalWrite(17, LOW);
 
   unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis >= interval) {
+
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
 
-    float capValue = _capSens.readDiffCapacitance();
+    float capValue = _capSens.readCapacitance();
+
+    capBuffer.push(capValue);
+
+    float maxCap = 0;
+    for (int i = 0; i < capBuffer.size(); i++){
+      if(maxCap < capBuffer[i]) maxCap = capBuffer[i];
+    }
+
     float tempValue = _tempSens.readTemperature();
 
     capPacket.len = 0;
     Comms::packetAddFloat(&capPacket, capValue);
-    Comms::packetAddFloat(&capPacket, capValue);
+    Comms::packetAddFloat(&capPacket, maxCap);
     Comms::packetAddFloat(&capPacket, tempValue);
     Comms::emitPacket(&capPacket);
   }
