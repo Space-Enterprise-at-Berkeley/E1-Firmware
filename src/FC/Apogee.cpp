6bf956887542c6db8b25b6c80adf606cbaa590ab
engineerboi <91240037+engineerboi@users.noreply.github.com>
2022-03-16 18:46:36 -0700

    apogee detection go brr once again

--- src/FC/Apogee.cpp
+++ src/FC/Apogee.cpp
@@ -2,102 +2,30 @@
 #include "Barometer.h"
 #include "IMU.h"
 
-namespace Apogee {
-    uint32_t apogeeUpdatePeriod = 100 * 1000;
-    Comms::Packet apogeePacket = {.id = 4};
-    
-    float mainParachuteAltitude = 950; // feet altitude for deployment of main? 
-    
-    // float accelerationThreshold = -2;  // change this and check units?
-    float velocityThreshold = -2; // change this and check units?
-    
-    void initApogee(){
-    }
-    
-    uint32_t apogeeDetectionTask(uint8_t packetID,float *value) {
-        apogeePacket.id = packetID;
-        apogeePacket.len = 0;
-
-        // barometerAcceleration = altitudeToAcceleration(Barometer::altitude);
-        barometerVelocity = altitudeToVelocity(Barometer::altitude);
-        
-        if (barometerVelocity < velocityThreshold) {
-            // deploy drogue
-            apogee = 1;
-        }
-
-        if (apogee == 1 && Barometer::altitude < mainParachuteAltitude){
-            // deploy main chute
-            mainChuteDeploy = 1;
-        }
-
-
-        Comms::packetAddUint8(&apogeePacket, apogee);
-        Comms::packetAddUint8(&apogeePacket, mainChuteDeploy);
-        Comms::emitPacket(&apogeePacket);
-
-        return apogeeUpdatePeriod;
-    }
-
-    float altitudeToAcceleration(float altitude) {
-        float velocity = (altitude - previousAltitude) / barometerSampleRate;
-        float barometerAcceleration = (velocity - previousVelocity) / barometerSampleRate;
-
-        previousAltitude = altitude;
-        previousVelocity = velocity;
-
-        return barometerAcceleration;
-   
-
-    
-    float altitudeToVelocity(float altitude) {
-        float velocity = (altitude - previousAltitude) / barometerSampleRate;
-
-        previousAltitude = altitude;
-        previousVelocity = velocity;
-
-        return barometerVelocity;
-    }
-}
-
-
-
-    // check for barometer and imu flag (to check for data continuity)
-
-    // use dh2/d2t to find acceleration for barometer
-    // 
-
-    // // compare barometer values to model via kalman filter (if not flagged)
-    // // do same thing for imu
-
-    // somehow average/weight imu and barometer acceleration values
-
-    // find average wobble degree, 
-
-    // if angle > threshold(~30 degrees) and barometer derived dh/dt <0 deploy parachute
-
-
-
-
-
-
-
-// //////
-
-#include "Apogee.h"
-#include "Barometer.h"
-#include "IMU.h"
-#include <vector>
 
 namespace Apogee {
     uint32_t apogeeUpdatePeriod = 100 * 1000;
     Comms::Packet apogeePacket = {.id = 4};
     
     float mainParachuteAltitude = 950; // feet altitude for deployment of main? 
-    float datapointsCount = 0;
+    uint32_t apogeeTime = 0;
+    int apogee = 0;
     float apogeeCheck = 0;
     float launchCheck = 0;
-    float stepAvg = 25;
+    const float stepAvg = 25;
+    uint32_t mainChuteDeploy = 0;
+    uint32_t mainChuteDeployTime;
+    float velocitySamples[stepAvg] = {0};
+    int velocityIndex = 0;
+    float previousAltitude = 0;
+    float previousVelocity = 0;
+    float barometerVelocity = 0;
+    float velocityAvg = 0;
+    
+
+    
+    
+
     void initApogee(){
     }
     
@@ -106,35 +34,44 @@ namespace Apogee {
         apogeePacket.len = 0;
 
         barometerVelocity = altitudeToVelocity(Barometer::altitude);
-        velocityArray[datapointsCount] = barometerVelocity
-        velocityAvg = 
+        velocitySamples[velocityIndex] = barometerVelocity;
+        
+        float runningSum = 0;
 
-        if (barometerVelocity < velocityThreshold) {
-            // deploy drogue
-            apogee = 1;
+        for (int i = 0; i < stepAvg; i++) {
+            runningSum = runningSum + velocitySamples[i];
         }
 
-        if (apogee == 1 && Barometer::altitude < mainParachuteAltitude){
+        velocityAvg = runningSum / stepAvg;
+        
+        if (Barometer::altitude > 700) {
+            launchCheck = 1;
+        }
+
+        if ((velocityAvg < 0 ) && (apogeeCheck == 0) && (launchCheck == 1))  {
+            apogee = 1;
+            apogeeCheck = 1;
+            apogeeTime = millis()
+
+
+            ///send info to deploy drogue
+        }
+
+        if ((apogee == 1) && (Barometer::altitude < mainParachuteAltitude) && (mainChuteDeploy == 0)){
             // deploy main chute
             mainChuteDeploy = 1;
+            mainChuteDeployTime = millis();
         }
+        velocityIndex = (velocityIndex + 1) % stepAvg
 
 
-        Comms::packetAddUint8(&apogeePacket, apogee);
-        Comms::packetAddUint8(&apogeePacket, mainChuteDeploy);
+        Comms::packetAddUint32(&apogeePacket, apogeeTime);
+        Comms::packetAddUint32(&apogeePacket, mainChuteDeployTime);
         Comms::emitPacket(&apogeePacket);
 
         return apogeeUpdatePeriod;
     }
 
-    float altitudeToAcceleration(float altitude) {
-        float velocity = (altitude - previousAltitude) / barometerSampleRate;
-        float barometerAcceleration = (velocity - previousVelocity) / barometerSampleRate;
-
-        previousAltitude = altitude;
-        previousVelocity = velocity;
-
-        return barometerAcceleration;
     
     float altitudeToVelocity(float altitude) {
         float velocity = (altitude - previousAltitude) / barometerSampleRate;
@@ -144,35 +81,24 @@ namespace Apogee {
 
         return barometerVelocity;
     }
-    float avgVelocity(vector<float> velocityVector, int stepAvg) {
-    int vectSize = velocityVector.size();
-    float velocityAvg = (velocityVector[vectSize - 1] + velocityVector[vectSize - 2] + 
-                        velocityVector[vectSize - 3] + velocityVector[vectSize - 4] + 
-                        velocityVector[vectSize - 5] + velocityVector[vectSize - 6] + 
-                        velocityVector[vectSize - 7] + velocityVector[vectSize - 8] + 
-                        velocityVector[vectSize - 9] + velocityVector[vectSize - 10] + 
-                        velocityVector[vectSize - 11] + velocityVector[vectSize - 12] + 
-                        velocityVector[vectSize - 13] + velocityVector[vectSize - 14] + 
-                        velocityVector[vectSize - 15] + velocityVector[vectSize - 16] + 
-                        velocityVector[vectSize - 17] + velocityVector[vectSize - 18] + 
-                        velocityVector[vectSize - 19] + velocityVector[vectSize - 20])/stepAvg;
 
-    return velocityAvg;
+
+
+
+    
+    // float avgVelocity(vector<float> velocityVector, int stepAvg) {
+    // int vectSize = velocityVector.size();
+    // float velocityAvg = (velocityVector[vectSize - 1] + velocityVector[vectSize - 2] + 
+    //                     velocityVector[vectSize - 3] + velocityVector[vectSize - 4] + 
+    //                     velocityVector[vectSize - 5] + velocityVector[vectSize - 6] + 
+    //                     velocityVector[vectSize - 7] + velocityVector[vectSize - 8] + 
+    //                     velocityVector[vectSize - 9] + velocityVector[vectSize - 10] + 
+    //                     velocityVector[vectSize - 11] + velocityVector[vectSize - 12] + 
+    //                     velocityVector[vectSize - 13] + velocityVector[vectSize - 14] + 
+    //                     velocityVector[vectSize - 15] + velocityVector[vectSize - 16] + 
+    //                     velocityVector[vectSize - 17] + velocityVector[vectSize - 18] + 
+    //                     velocityVector[vectSize - 19] + velocityVector[vectSize - 20])/stepAvg;
+
+    // return velocityAvg;
 }
-
-
-
-    // check for barometer and imu flag (to check for data continuity)
-
-    // use dh2/d2t to find acceleration for barometer
-    // 
-
-    // // compare barometer values to model via kalman filter (if not flagged)
-    // // do same thing for imu
-
-    // somehow average/weight imu and barometer acceleration values
-
-    // find average wobble degree, 
-
-    // if angle > threshold(~30 degrees) and barometer derived dh/dt <0 deploy parachute
 
--- src/FC/Apogee.h
+++ src/FC/Apogee.h
@@ -7,21 +7,24 @@
 #include <Arduino.h>
 
 namespace Apogee {
-    extern uint32_t apogeeDetectionPeriod;
-
     extern float previousAltitude;
     extern float barometerSampleRate;
-    extern float previousVelocity;
-    extern float accelerationThreshold;
-    extern float interpretedAcceleration;
-    extern float barometerAcceleration;
     extern float barometerVelocity;
-    extern float velocityThreshold;
     extern uint8_t apogee;
     extern float mainParachuteAltitude;
     extern uint8_t mainChuteDeploy;
+
+    extern float apogee; // this equals the time registered for apogee
+    extern int apogeeCheck; // if 1, disallows entry into if statement
+    extern int launchCheck; // becomes 1 when over 700 feet altitude
+    extern int stepAvg; // defines how many values we average over
+    extern float velocityAvg; // current velocity resulting from averaging past ___ (stepAvg) velocity values
+    extern float apogeeTime; // stores the time at which apogee is detected/drogue deployed
+    extern int mainChuteDeploy; // if 1, disallows entry into if statement
     
     void initApogee();
-    float altitudeToAcceleration(float altitude);
+    float altitudeToVelocity(float altitude);
     uint32_t apogeeDetectionTask();
+    // float avgVelocity(vector<float> velocityVector, int stepAvg)
+
 };
