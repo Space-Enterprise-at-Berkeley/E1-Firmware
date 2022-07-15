54e268b8bb67937473b8d5d6949f9388c81b87b5
ctychen <34198336+ctychen@users.noreply.github.com>
2022-07-15 15:23:52 -0700

    E1 extension ADC test

---
+++ src/tests/e1-adc-test/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+#include <ADS8167.h>
+
+ADS8167 adc;
+
+int main(){
+    // cs, ready, alert
+    Serial.begin(115200);
+    while(!Serial);
+    adc.init(&SPI, 37, 25, 999);
+    adc.setAllInputsSeparate();
+    adc.enableOTFMode();
+
+    while(1) {
+        Serial.print("  adc0:   ");
+        Serial.print(adc.readChannelOTF(0));
+        Serial.print("  adc1:   ");
+        Serial.print(adc.readChannelOTF(1));
+        Serial.print("  adc2 :    ");
+        Serial.print(adc.readChannelOTF(2));
+        Serial.print("  adc3:   ");
+        Serial.print(adc.readChannelOTF(3));
+        Serial.print("  adc4: 	");
+        Serial.print(adc.readChannelOTF(4));
+        Serial.print("  adc5:   ");
+        Serial.print(adc.readChannelOTF(5));
+        Serial.print("  adc6: ");
+        Serial.print(adc.readChannelOTF(6));
+        Serial.print("  adc7:   ");
+        Serial.println(adc.readChannelOTF(7));
+        delay(200);
+    }
+    return 0;
+}
