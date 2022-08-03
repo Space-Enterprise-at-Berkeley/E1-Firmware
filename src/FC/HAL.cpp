f63b486169c822d8d4eae11499f8e8ade68483dd
ctychen <34198336+ctychen@users.noreply.github.com>
2022-08-02 23:17:51 -0700

    testing stack

--- src/FC/HAL.cpp
+++ src/FC/HAL.cpp
@@ -10,22 +10,6 @@ namespace HAL {
 
     TCA6408A ioExpander;
 
-    INA219 chan0;
-    INA219 chan1;
-    INA219 chan2;
-    INA219 chan3;
-    INA219 chan4;
-    INA219 chan5;
-    INA219 chan6;
-    INA219 chan7;
-    INA219 chan8;
-    INA219 chan9;
-    INA219 chan10;
-    INA219 chan11;
-    INA219 chan12;
-    INA219 chan13;
-    INA219 chan14;
-
     //Voltage and Current Sense Mux
     Mux valveMux = {.muxSelect1Pin = 3,
                     .muxSelect2Pin = 4,
@@ -51,10 +35,11 @@ namespace HAL {
     MuxChannel muxChan14;
     MuxChannel muxChan15;
 
-    MCP9600 tcAmp0;
-    MCP9600 tcAmp1;
-    MCP9600 tcAmp2;
-    MCP9600 tcAmp3;
+    // Not usable rn
+    // MCP9600 tcAmp0;
+    // MCP9600 tcAmp1;
+    // MCP9600 tcAmp2;
+    // MCP9600 tcAmp3;
 
     void initChannel(INA219 *channel, uint8_t address) {
         channel->init(&Wire1, address, chanShuntR, chanCurrMax, INA219_RANGE_32V, INA219_GAIN_160MV, INA219_BUS_RES_12BIT, INA219_SHUNT_RES_12BIT_1S, INA219_MODE_SHUNT_BUS_CONT);
@@ -97,25 +82,26 @@ namespace HAL {
         // IO Expander
         ioExpander.init(0x20, &Wire);
 
+        // todo: channels now don't have ina219's
         // valves
-        initChannel(&chan0, 0x40);
-        initChannel(&chan1, 0x41);
-        initChannel(&chan2, 0x42);
-        initChannel(&chan3, 0x43);
-        initChannel(&chan4, 0x44);
-        initChannel(&chan5, 0x45);
+        // initChannel(&chan0, 0x40);
+        // initChannel(&chan1, 0x41);
+        // initChannel(&chan2, 0x42);
+        // initChannel(&chan3, 0x43);
+        // initChannel(&chan4, 0x44);
+        // initChannel(&chan5, 0x45);
         // heaters
-        initChannel(&chan6, 0x49);
-        initChannel(&chan7, 0x4A);
-        initChannel(&chan8, 0x4B);
-        initChannel(&chan9, 0x4C);
-        initChannel(&chan10, 0x4D);
-        initChannel(&chan11, 0x4E);
+        // initChannel(&chan6, 0x49);
+        // initChannel(&chan7, 0x4A);
+        // initChannel(&chan8, 0x4B);
+        // initChannel(&chan9, 0x4C);
+        // initChannel(&chan10, 0x4D);
+        // initChannel(&chan11, 0x4E);
         // chute
-        initChannel(&chan12, 0x48);
-        initChannel(&chan13, 0x47);
+        // initChannel(&chan12, 0x48);
+        // initChannel(&chan13, 0x47);
         // spare
-        initChannel(&chan14, 0x46);
+        // initChannel(&chan14, 0x46);
 
         // channels
         pinMode(chan0Pin, OUTPUT);
@@ -126,10 +112,11 @@ namespace HAL {
         pinMode(chan5Pin, OUTPUT);
 
         // thermocouple amplifiers
-        tcAmp0.init(0x60, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
-        tcAmp1.init(0x61, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
-        tcAmp2.init(0x62, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
-        tcAmp3.init(0x63, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
+        // 08/02: not on e1 ext
+        // tcAmp0.init(0x60, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
+        // tcAmp1.init(0x61, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
+        // tcAmp2.init(0x62, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
+        // tcAmp3.init(0x63, &Wire, MCP9600_ADCRESOLUTION_16, MCP9600_TYPE_K, 0);
 
         muxChan0.init(&valveMux, 0);
         muxChan1.init(&valveMux, 1);
