609d6b453d75a967eaa98691a3f84c386849eb7d
Curtis Hu <43627632+curtisjhu@users.noreply.github.com>
2022-09-22 18:44:44 -0700

    IMU ready to be reviewed

--- src/FC/IMU.cpp
+++ src/FC/IMU.cpp
@@ -1,10 +1,38 @@
 #include "IMU.h"
 
 namespace IMU {
-	void init() {
-		Adafruit_BNO055::begin()
-	};
-	uint32_t sampleIMU() {
+	BNO055 bno055;
 
+	void init() {
+		bno055.begin()
 	};
+
+	uint32_t sampleIMU() {
+		auto a = bno055.getVector(BNO055::VECTOR_LINEARACCEL);
+		auto q = bno055.getQuat();
+
+		float accelX, accelY, accelZ;
+		float quatX, quatY, quatZ, quatW;
+
+		accelX = a.x(), accelY = a.y(), accelZ = a.z();
+		quatX = q.x(), quatY = q.y(), quatZ = q.z(), quatW = q.w();
+
+		Comms::Packet imuPacket = { .id = 1234, .len = 0};
+
+		Comms::packetAddFloat(&imuPacket, accelX);
+		Comms::packetAddFloat(&imuPacket, accelY);
+		Comms::packetAddFloat(&imuPacket, accelZ);
+
+		Comms::packetAddFloat(&imuPacket, quatX);
+		Comms::packetAddFloat(&imuPacket, quatY);
+		Comms::packetAddFloat(&imuPacket, quatZ);
+		Comms::packetAddFloat(&imuPacket, quatW);
+
+		Comms::emitPacket(&imuPacket);
+
+		// update every 3 seconds
+		uint32_t updatePeriod = 3 * 1000;
+		return updatePeriod;
+	};
+
 }
--- src/FC/IMU.h
+++ src/FC/IMU.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <BNO055.h>
+#include <Comms.h>
 
 namespace IMU {
+	extern updatePeriod;
+
 	void init();
 	uint32_t sampleIMU();
 }
