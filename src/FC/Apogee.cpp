ba89bde43b9f7c5298cebfd05f614fd3ec0817d3
BuildTools <zrdenning@gmail.com>
2022-09-27 15:25:50 -0700

    More apogee stuff

--- src/FC/Apogee.cpp
+++ src/FC/Apogee.cpp
@@ -12,14 +12,22 @@ Detection methods:
 
 namespace Apogee {
 
-	const uint32_t millis = 10;
+	const uint32_t updatePeriod = 10;
+	bool beginDetection = false;
+
+	uint32_t lastReading;
 
 	void init() {
 
 	}
 
 	uint32_t checkForApogee() {
+		uint32_t currentReading = millis();
+		uint32_t delta_t = currentReading - lastReading;
+		lastReading = currentReading;
+		Comms::Packet apogeePacket = {.id = 1111};
 
-		return millis;
+		Comms::emitPacket(&apogeePacket);
+		return updatePeriod;
 	}
 }
