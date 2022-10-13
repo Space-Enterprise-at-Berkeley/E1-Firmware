49fe07657a7b600943fcd704e28c3caddb8ac1ef
Gabriel Taboada <gabtab@berkeley.edu>
2022-10-12 18:53:01 -0700

    fixed all Black Box errors

--- src/FC/BlackBox.cpp
+++ src/FC/BlackBox.cpp
@@ -1,15 +1,12 @@
 #include "BlackBox.h"
     namespace BlackBox {
-        const uint32_t updatePeriod = 100 * 1000; //TODO update period
         uint8_t lastElement = 0;
         uint8_t page = 0;
         byte pageAddress = 0;
         bool full = false;
         W25Q w25q;
-
-        // buffer has 20 packets, each packet has 66 uint8_t, TODO, confirm packet size;
-        uint8_t bufferSize = 20*(64 + 2);
-        volatile uint8_t buffer[bufferSize]; //TODO should mem be allocated here or in init?
+        const int bufferSize = 20*(64 + 2);
+        volatile uint8_t buffer[bufferSize]; 
 
         /*******************************Public Methods******************************/
 
@@ -19,9 +16,10 @@
         * @brief initlizes W25Q Memory chip and clears buffer
         *****************************************************************************/
 
-        void init() {
-            w25q.init()
-            clearBuffer()
+        void init(int FLASH_SS) {
+            w25q.init(FLASH_SS);
+            clearBuffer();
+            Comms::registerEmitter(writeToBuffer);
         }
 
         /*****************************************************************************
@@ -35,22 +33,17 @@
         * Description: @brief writes the packet to buffer, once the buffer is full 
         * buffer is writen onto W25Q. Amortized O(n).
         *****************************************************************************/
-        uint32_t writeToBuffer(Comms::Packet packet) {
-            if(lastElement >= bufferSize) {
-                writeBufferToBB()
+        void writeToBuffer(Comms::Packet packet, uint8_t ip) {
+            if(lastElement + packet.len + 8 >= bufferSize) {
+                writeBufferToBB();
+                clearBuffer();
                 lastElement = 0;
             }
-            //memecpy(&buffer,&packet, 66) TODO, use memcpy for nicer code
-            
-            for(int i = 0; i < packet.len + 8 ; i++) {
-                buffer[lastElement] = uInt;
-                lastElement++;
-            }
-            
-            return updatePeriod;
-
+                memcpy(buffer + lastElement, &packet, packet.len + 8);
+                lastElement += packet.len + 8;
         }
         /*******************************Private Methods******************************/
+
         /*****************************************************************************
         * Function: WriteBufferToBB
         *
@@ -58,23 +51,23 @@
         *  into chip and closes reader   
         *****************************************************************************/
         void writeBufferToBB() {
-            if (Full) {
+            if (full) {
                 return;
             }
-            W25Q::initStreamWrite(page, pageAddress)
-            for (uint8_t uInt: buffer) {
+            w25q.initStreamWrite(page, pageAddress);
+            for (uint8_t elem: buffer) {
                 pageAddress++;
-                if (pageAddress > 256 ) {
-                    pageAdress = pageAdress % 256;
+                if (pageAddress > w25q.pageSize - 1) {
+                    pageAddress = pageAddress % 256;
                     if (page > 65536) {
-                        Full = true;
+                        full = true;
                     }
                     page++;
-                    fc.streamWrite(uInt);//TODO, how to write all bytes in the package
+                    w25q.streamWrite(elem);
 
                 }
             }
-            w25q.closeStreamWrite()
+            w25q.closeStreamWrite();
         }
         /*****************************************************************************
         * Function: clearBuffer
@@ -94,6 +87,6 @@
         *****************************************************************************/
         void resetAll() {
             clearBuffer();
-            fc.chipErase();
+            w25q.chipErase();
         }
     }
--- src/FC/BlackBox.h
+++ src/FC/BlackBox.h
@@ -5,17 +5,8 @@
 
 namespace BlackBox {
 
-    public:
-        void init()
-        uint32_t WriteToBuffer();
-    private:
-        const uint32_t updatePeriod;
-        const uint8_t bufferSize;
-        uint8_t lastElement;
-        uint8_t page;
-        bool Full;
-        byte pageAddress;
-        volatile Comms::Packet buffer[];
-        void writeBufferToBB()
-        void clearBuffer()
+        void init();
+        void writeToBuffer();  
+        void clearBuffer();   
+        void writeBufferToBB();
 }
