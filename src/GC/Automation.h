8051b096c1f36b2b98d9ac631cc7253aabf3bb99
Andy Cate <and164v@gmail.com>
2022-10-13 22:50:57 -0700

    updated GC code

--- src/GC/Automation.h
+++ src/GC/Automation.h
@@ -13,17 +13,9 @@ namespace Automation {
     const float currentThreshold = 0.2;
     const float mainValveCurrentThreshold = 0.08;
 
-    const float thermocoupleAbsoluteThreshold = 200;
-    const float thermocoupleThreshold = 150;
-    const float thermocoupleRateThreshold = 20;
-
-    const float loadCellThreshold = 100.0; //TODO actual value 100
-    const float loadCellSampleRate = 0.0125;
 
     extern Task *flowTask; // perform burn
     extern Task *abortFlowTask;
-    extern Task *checkForTCAbortTask;
-    extern Task *checkForLCAbortTask;
 
     extern uint32_t loxLead;
     extern uint32_t burnTime;
@@ -33,16 +25,13 @@ namespace Automation {
     extern bool igniterTriggered;
     extern bool breakwireBroken;
 
-    void initAutomation(Task *flowTask, Task *abortFlowTask, Task *checkForTCAbortTask, Task *checkForLCAbortTask);
+    void initAutomation(Task *flowTask, Task *abortFlowTask);
     void beginFlow(Comms::Packet packet, uint8_t ip);
     uint32_t flow();
     void beginManualAbortFlow(Comms::Packet packet, uint8_t ip);
     void beginAbortFlow();
     uint32_t abortFlow();
     uint32_t checkIgniter();
-    void readLoadCell(Comms::Packet packet, uint8_t ip);
-    uint32_t checkForTCAbort();
-    uint32_t checkForLCAbort();
 
     void handleAutoSettings(Comms::Packet recv, uint8_t id);
 };
