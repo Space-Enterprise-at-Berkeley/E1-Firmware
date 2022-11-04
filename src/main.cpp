9729599d090bcfdb83ab9794894a833bf17bba3a
Andy Cate <and164v@gmail.com>
2022-11-03 17:51:40 -0700

    add code to auto space packets

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,12 @@ TMP236 _tempSens = TMP236(TEMP_PIN);
 
 char rs485Buffer[sizeof(Comms::Packet)];
 int cnt = 0;
+int indicatorDuty = 500;
+int indicatorPeriod = 1000;
+int indicatorLastTime = 0;
+
+const int timeBetweenTransmission = 100; // ms
+int lastTransmissionTime = 0;
 
 void setup()
 {
@@ -43,6 +49,10 @@ void setup()
   digitalWrite(TE_485, LOW);
   #endif
   digitalWrite(STATUS_LED, LOW);
+
+  #ifdef FUEL
+  delay(50);
+  #endif
 }
 
 unsigned long previousMillis = 0;
@@ -56,36 +66,55 @@ Comms::Packet capPacket = {.id = PACKET_ID};
 
 void loop()
 {
-  while(Serial1.available()) {
-    rs485Buffer[cnt] = Serial1.read();
-    // DEBUG((uint8_t)rs485Buffer[cnt]);
-    // DEBUG(" ");
-    // DEBUG_FLUSH();
-    if(cnt == 0 && rs485Buffer[cnt] != PACKET_ID) {
-      break;
-    }
+  // while(Serial1.available()) {
+  //   rs485Buffer[cnt] = Serial1.read();
+  //   // DEBUG((uint8_t)rs485Buffer[cnt]);
+  //   // DEBUG(" ");
+  //   // DEBUG_FLUSH();
+  //   if(cnt == 0 && rs485Buffer[cnt] != PACKET_ID) {
+  //     break;
+  //   }
 
-    if(rs485Buffer[cnt] == '\n') {
-      Comms::Packet *packet = (Comms::Packet *)&rs485Buffer;
-      if(Comms::verifyPacket(packet)) {
-        digitalWrite(STATUS_LED, HIGH);
-        cnt = 0;
+  //   if(rs485Buffer[cnt] == '\n') {
+  //     Comms::Packet *packet = (Comms::Packet *)&rs485Buffer;
+  //     if(Comms::verifyPacket(packet)) {
+  //       cnt = 0;
 
-        // Comms::emitPacket(&capPacket, &Serial);
-        digitalWrite(EN_485, HIGH);
-        Comms::emitPacket(&capPacket, &Serial1);
-        Serial1.flush();
-        digitalWrite(EN_485, LOW);
-        digitalWrite(STATUS_LED, LOW);
-        break;
-      }
-    }
-    cnt++;
+  //       // Comms::emitPacket(&capPacket, &Serial);
+  //       digitalWrite(EN_485, HIGH);
+  //       Comms::emitPacket(&capPacket, &Serial1);
+  //       Serial1.flush();
+  //       digitalWrite(EN_485, LOW);
+  //       break;
+  //     }
+  //   }
+  //   cnt++;
 
-    if(cnt >= 20) {
-      cnt = 0;
+  //   if(cnt >= 20) {
+  //     cnt = 0;
+  //   }
+  // }
+  if(millis() - lastTransmissionTime >= timeBetweenTransmission) {
+    DEBUG("Transmitting ");
+    DEBUG(Comms::packetGetFloat(&capPacket, 0));
+    DEBUG("\n");
+    DEBUG_FLUSH();
+    lastTransmissionTime = lastTransmissionTime + timeBetweenTransmission;
+    digitalWrite(EN_485, HIGH);
+    Comms::emitPacket(&capPacket, &Serial1);
+    Serial1.flush();
+    digitalWrite(EN_485, LOW);
+  }
+
+  #ifdef FUEL
+  if(Serial1.available() && millis() - lastTransmissionTime >= timeBetweenTransmission/4) {
+    DEBUG("R\n");
+    lastTransmissionTime = millis() - timeBetweenTransmission/2;
+    while(Serial1.available()) {
+      Serial1.read();
     }
   }
+  #endif
 
 
 
@@ -97,6 +126,12 @@ void loop()
     previousMillis = currentMillis;
 
     float capValue = _capSens.readCapacitance();
+    if(capValue < 0.0) {
+      // error reading from sensor
+      indicatorDuty = 200;
+    } else {
+      indicatorDuty = 500;
+    }
     DEBUG(capValue);
     DEBUG('\n');
     DEBUG_FLUSH();
@@ -115,6 +150,7 @@ void loop()
     Comms::packetAddFloat(&capPacket, capValue);
     Comms::packetAddFloat(&capPacket, avgCap);
     Comms::packetAddFloat(&capPacket, tempValue);
+    // Comms::packetAddFloat(&capPacket, 0.0);
     
     uint32_t timestamp = millis();
     capPacket.timestamp[0] = timestamp & 0xFF;
@@ -126,5 +162,14 @@ void loop()
     uint16_t checksum = Comms::computePacketChecksum(&capPacket);
     capPacket.checksum[0] = checksum & 0xFF;
     capPacket.checksum[1] = checksum >> 8;
+  }
+
+  int timeNow = currentMillis = millis();
+  if(timeNow - indicatorLastTime >= indicatorDuty) {
+    digitalWrite(STATUS_LED, HIGH);
+  }
+  if(timeNow - indicatorLastTime >= indicatorPeriod) {
+    digitalWrite(STATUS_LED, LOW);
+    indicatorLastTime = timeNow;
   }
 }
