611f9bda9d1c21429c337e21612a8ed15021b992
Andy Cate <and164v@gmail.com>
2022-11-08 00:16:17 -0800

    mod test script

--- src/tests/transmittertest.cpp
+++ src/tests/transmittertest.cpp
@@ -1,20 +1,39 @@
 #include <Arduino.h>
 
-#define RS485_SERIAL Serial1
+#include <Comms.h>
+
 #define RS485_SW_PIN 22 
 
 void setup() {
-    Serial.begin(115200);
-    RS485_SERIAL.begin(921600);
+    Serial.begin(921600);
     pinMode(RS485_SW_PIN, OUTPUT);
     digitalWrite(RS485_SW_PIN, HIGH);
+    pinMode(5, OUTPUT);
+    digitalWrite(5, LOW);
 }
 
 void loop() {
-    for(int i = 0; i < 276; i++) {
-        RS485_SERIAL.write('\n');
+    // Comms::Packet p = {.id = 38};
+    // Comms::packetAddFloat(&p, 2.3);
+    // Comms::packetAddFloat(&p, 4.5);
+    // Comms::packetAddFloat(&p, 6.7);
+
+    // uint32_t timestamp = millis();
+    // p.timestamp[0] = timestamp & 0xFF;
+    // p.timestamp[1] = (timestamp >> 8) & 0xFF;
+    // p.timestamp[2] = (timestamp >> 16) & 0xFF;
+    // p.timestamp[3] = (timestamp >> 24) & 0xFF;
+
+    // //calculate and append checksum to struct
+    // uint16_t checksum = Comms::computePacketChecksum(&p);
+    // p.checksum[0] = checksum & 0xFF;
+    // p.checksum[1] = checksum >> 8;
+
+    // Comms::emitPacket(&p, &Serial);
+    for(int i = 0; i < 3000; i++) {
+        Serial.write('\n');
     }
-    RS485_SERIAL.flush();
-    Serial.println("yeet");
-    delay(1000);
+    Serial.flush();
+
+    delay(30);
 }
