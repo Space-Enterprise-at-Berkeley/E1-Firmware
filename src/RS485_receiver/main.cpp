cf8a84ceee8b32b139b584a4bc40e660036fa9a4
samhitag3 <samhita.ghosh@berkeley.edu>
2023-02-24 17:08:29 -0800

    rs 485

--- src/RS485_receiver/main.cpp
+++ src/RS485_receiver/main.cpp
@@ -7,7 +7,7 @@
 
 #define LOX_SERIAL Serial5 // TODO: verify this
 #define LOX_REN_PIN 22
-#define FUEL_SERIAL Serial4 // TODO: verify this
+#define FUEL_SERIAL Serial3 // TODO: verify this
 #define FUEL_REN_PIN 18
 
 char loxBuffer[sizeof(Comms::Packet)];
@@ -24,7 +24,7 @@ void setup() {
     while(!Serial) {} // wait for user to open serial port (debugging only)
     #endif
 
-    // Comms::initComms();
+    Comms::initComms();
 
     DEBUG("STARTING UP\n");
     DEBUG_FLUSH();
@@ -37,42 +37,48 @@ void setup() {
 }
 
 void loop() {
+        // if (LOX_SERIAL.available()) {
+        //     Serial.println("hello world");
+        // }
 
-        while(LOX_SERIAL.available()) {
-            loxBuffer[loxCnt] = LOX_SERIAL.read();
-            loxCnt++;
-        }
-        Serial.write(loxBuffer, loxCnt);
-        loxCnt = 0;
+        // if (FUEL_SERIAL.available()) {
+        //     Serial.println("hello fuel world");
+        // }
 
-        while(FUEL_SERIAL.available()) {
-            fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
-            fuelCnt++;
-        }
-        Serial.write(fuelBuffer, fuelCnt);
-        fuelCnt = 0;
+        // while(LOX_SERIAL.available()) {
+        //     loxBuffer[loxCnt] = LOX_SERIAL.read();
+        //     loxCnt++;
+        // }
+        // Serial.write(loxBuffer, loxCnt);
+        // loxCnt = 0;
 
-        // while(FUEL_SERIAL.available() && fuelCnt < 256) {
+        // while(FUEL_SERIAL.available()) {
         //     fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
-        //     if(fuelBuffer[fuelCnt] == '\n') {
-        //         Comms::Packet *packet = (Comms::Packet *)&fuelBuffer;
-        //         if(Comms::verifyPacket(packet)) {
-        //             fuelCnt = 0;
-        //             DEBUG("fuel: ");
-        //             DEBUG(packet->id);
-        //             DEBUG(" : ");
-        //             DEBUG(Comms::packetGetFloat(packet, 0));
-        //             DEBUG("\n");
-        //             DEBUG_FLUSH();
-        //             Comms::emitPacket(packet);
-        //             break;
-        //         }
-        //     }
         //     fuelCnt++;
         // }
-        // if(fuelCnt == 256) {
-        //     DEBUG("RESETTING FUEL BUFFER\n");
-        //     fuelCnt = 0;
-        // }
+        // Serial.write(fuelBuffer, fuelCnt);
+        // fuelCnt = 0;
+        while(FUEL_SERIAL.available() && fuelCnt < 256) {
+            fuelBuffer[fuelCnt] = FUEL_SERIAL.read();
+            if(fuelBuffer[fuelCnt] == '\n') {
+                Comms::Packet *packet = (Comms::Packet *)&fuelBuffer;
+                if(Comms::verifyPacket(packet)) {
+                    fuelCnt = 0;
+                    DEBUG("fuel: ");
+                    DEBUG(packet->id);
+                    DEBUG(" : ");
+                    DEBUG(Comms::packetGetFloat(packet, 0));
+                    DEBUG("\n");
+                    DEBUG_FLUSH();
+                    Comms::emitPacket(packet);
+                    break;
+                }
+            }
+            fuelCnt++;
+        }
+        if(fuelCnt == 256) {
+            DEBUG("RESETTING FUEL BUFFER\n");
+            fuelCnt = 0;
+        }
 
     }
