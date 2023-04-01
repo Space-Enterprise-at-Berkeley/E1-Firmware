db5393807a2be3d52c4ec69aec48cf5be392b746
Andy Cate <and164v@gmail.com>
2023-04-01 15:37:00 -0700

    updated transceiver code

--- src/RS485_receiver/main.cpp
+++ src/RS485_receiver/main.cpp
@@ -5,8 +5,8 @@
 
 #define TASK_COUNT (sizeof(taskTable) / sizeof(struct Task))
 
-#define LOX_SERIAL Serial5 // TODO: verify this
-#define LOX_REN_PIN 22
+#define LOX_SERIAL Serial4 // TODO: verify this
+#define LOX_REN_PIN 19
 #define FUEL_SERIAL Serial3 // TODO: verify this
 #define FUEL_REN_PIN 18
 
@@ -20,9 +20,6 @@ void setup() {
     Serial.begin(115200);
     LOX_SERIAL.begin(115200);
     FUEL_SERIAL.begin(115200);
-    #ifdef DEBUG_MODE
-    while(!Serial) {} // wait for user to open serial port (debugging only)
-    #endif
 
     Comms::initComms();
 
@@ -81,6 +78,35 @@ void loop() {
         if(fuelCnt == 256) {
             Serial.println("RESETTING FUEL BUFFER\n");
             fuelCnt = 0;
+        }
+
+
+        while(LOX_SERIAL.available() && loxCnt < 256) {
+            loxBuffer[loxCnt] = LOX_SERIAL.read();
+            if(loxCnt == 0 && ((uint8_t)loxBuffer[0] != 21 && (uint8_t) loxBuffer[0] != 22)) {
+                Serial.print((uint8_t)loxBuffer[0]);
+                Serial.print(" ");
+                continue;
+            }
+            if(loxBuffer[loxCnt] == '\n') {
+                Serial.println("got newline");
+                Comms::Packet *packet = (Comms::Packet *)&loxBuffer;
+                if(Comms::verifyPacket(packet)) {
+                    loxCnt = 0;
+                    Serial.print("lox: ");
+                    Serial.print(packet->id);
+                    Serial.print(" : ");
+                    Serial.print(Comms::packetGetFloat(packet, 0));
+                    Serial.print("\n");
+                    Comms::emitPacket(packet);
+                    break;
+                }
+            }
+            loxCnt++;
+        }
+        if(loxCnt == 256) {
+            Serial.println("RESETTING LOX BUFFER\n");
+            loxCnt = 0;
         }
 
     }
